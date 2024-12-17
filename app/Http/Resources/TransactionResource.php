c319795275e2ad7947c7d5250730ed440aacdf1b
Winzliu <alwin.liufandy@gmail.com>
2024-12-17 16:27:42 +0700

    fix(Transaction): Detailin result

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -15,16 +15,28 @@ class TransactionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'                => $this->id,
-            'wallet'            => $this->wallet->name,
-            'category'          => $this->category->name,
-            'amount'            => $this->amount,
-            'description'       => $this->description,
-            'transaction_date'  => $this->transaction_date,
-            'user'              => [
-                'fullname'      => $this->user->fullname,
-                'phone_number'  => $this->user->phone_number,
-                'email'         => $this->user->email,
+            'id'               => $this->id,
+            'wallet'           => [
+                'name'            => $this->wallet->name,
+                'initial_balance' => $this->wallet->initial_balance,
+                'total_income'    => $this->wallet->total_income ?? 0,
+                'total_expense'   => $this->wallet->total_expense ?? 0,
+                'total_balance'   => $this->wallet->total_balance ?? (($this->wallet->initial_balance ?? 0) + ($this->wallet->total_income ?? 0) - ($this->wallet->total_expense ?? 0)),
+                'is_active'       => $this->wallet->is_active
+            ],
+            'category'         => [
+                'name'        => $this->category->name,
+                'description' => $this->category->description,
+                'budget'      => $this->category->budget,
+                'type'        => $this->category->type
+            ],
+            'amount'           => $this->amount,
+            'description'      => $this->description,
+            'transaction_date' => $this->transaction_date,
+            'user'             => [
+                'fullname'     => $this->user->fullname,
+                'phone_number' => $this->user->phone_number,
+                'email'        => $this->user->email,
             ],
         ];
     }
